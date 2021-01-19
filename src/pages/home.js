fc14fc02ede553388ad31a38f6c9aaac4e8f2126
Natacha <natacha.har@onja.org>
2021-01-19 14:06:42 +0300

    Fixed the first page

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import JumbotronContainers from '../containers/jumbotron'
+
+export default function home() {
+    return (
+        <div>
+            <JumbotronContainers />
+        </div>
+    )
+}
